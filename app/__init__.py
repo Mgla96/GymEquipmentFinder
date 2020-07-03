247c7056aa4f9bfc9dedd0db12b98280db075371
mgla96 <mattgottlieb96@gmail.com>
2020-07-02 20:33:17 -0700

    running without db

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 from app import routes
 
-db = SQLAlchemy()
-
+#db = SQLAlchemy()
+#db.init_app(app)
+"""
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
@@ -14,4 +16,4 @@ def create_app():
         from . import routes #routes
         db.create_all() #create sql tables for data models
         return app
-
+"""
