05d078394079917439fc3ff9ec1cbf14fa351cae
mgla96 <mattgottlieb96@gmail.com>
2020-07-05 19:16:59 -0700

    bug testing

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,43 @@ def index():
 
 
 #python3 -m flask run
+
+"""
+scratch
+
+  {%for bar in bars%}
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-3">
+                    <span class="field-name">Name: </span>
+                    <span class="field-value">{{bar.name}}</span>
+                    <span class="field-name">Price: </span>
+                    <span class="field-value">{{bar.price}}</span>
+                    <span class="field-value">{{bar.image}}</span>
+                    <span class="field-name">Link: </span>
+                    <span class="field-value">{{bar.link}}</span>
+                    <span class="field-name">Available?: </span>
+                    <span class="field-value">{{bar.stock}}</span>
+                </div>
+                {%endfor%}
+
+                
+
+{%for plate in plates%}
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-3">
+                        <span class="field-name">Name: </span>
+                        <span class="field-value">{{plate.name}}</span>
+                        <span class="field-name">Price: </span>
+                        <span class="field-value">{{plate.price}}</span>
+                        <span class="field-value">{{plate.image}}</span>
+                        <span class="field-name">Link: </span>
+                        <span class="field-value">{{plate.link}}</span>
+                        <span class="field-name">Available?: </span>
+                        <span class="field-value">{{plate.stock}}</span>
+                    </div>
+                    {%endfor%}
+
+
+
+
+
+
+"""
