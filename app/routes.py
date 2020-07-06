1cf07ef91d5a824a69b4fb83b8b733cd91e2fbca
mgla96 <mattgottlieb96@gmail.com>
2020-07-05 19:07:41 -0700

    save

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,13 @@ from .models import db, Bars, Plates
 @app.route('/index')
 def index():
     #plates,barbells = [],[]
+    print("QUERY::::",Bars.query.all)
     user={
         'username':'Bill',
         'bars':Bars.query.all,
         'plates':Plates.query.all
     }
-    return render_template('index.html',title='Home',user=user)
+    return render_template('index.html',title='Home',user=user,bars=Bars.query.all,plates=Plates.query.all)
 
 
 #python3 -m flask run
