4ca6a38d24cfdcc7d4806cfd8eb587dbdc1384c9
mgla96 <mattgottlieb96@gmail.com>
2020-07-07 10:53:37 -0700

    https

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_talisman import Talisman
 
 app = Flask(__name__)
+Talisman(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
