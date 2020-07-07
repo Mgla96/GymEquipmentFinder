4a32fdcafb7e9e2ae796f1bb0e3a16be38bd1fb0
mgla96 <mattgottlieb96@gmail.com>
2020-07-06 20:48:54 -0700

    rep better price

--- app/scrape.py
+++ app/scrape.py
@@ -26,7 +26,6 @@ def scrpe():
         else:
             inStock=True
         tmp2 = Bars.query.filter_by(name=productName[:100]).update(dict(stock=inStock))
-        #print(tmp2)
         if tmp2>0:
             continue
         else:
@@ -38,12 +37,11 @@ def scrpe():
     response = get('https://www.roguefitness.com/weightlifting-bars-plates/bumpers')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     plates = html_soup.find_all('li',class_='item')
-
     for plate in plates:
         search_header = plate.find('div', class_='product-details')
-        productName = search_header.find('h2',class_='product-name').text
+        productName = search_header.find('h2',class_='product-name').textt
         productLink = search_header.find('a').get('href')     
-        productPrice = search_header.find('span',class_='price').text[1::]
+        productPrice = search_header.find('span',class_='price').text
         ownPage = get(productLink)
         html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
         page_container = html_soup2.find('div',class_='main-container')
@@ -62,7 +60,6 @@ def scrpe():
             db.session.add(tmp)
         db.session.commit()
   
-
     #REP Men's 20KG Barbell
     response = get('https://www.repfitness.com/bars-plates/olympic-bars')
     html_soup = BeautifulSoup(response.text, 'html.parser')
@@ -72,7 +69,7 @@ def scrpe():
         pricecont = bar.find('div', class_='price-container')
         productName = prodInfo.text
         productLink = prodInfo.find('a').get('href')
-        productPrice = pricecont.find('span',class_='price').text[1::]
+        productPrice = pricecont.find('span',class_='price').text
         ownPage = get(productLink)
         html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
         info = html_soup2.find('p',class_='availability')
@@ -83,11 +80,10 @@ def scrpe():
         else:
             stockbl=True
         tmp2 = Bars.query.filter_by(name=productName[:100]).update(dict(stock=stockbl))
-        #print(tmp2)
         if tmp2>0:
             continue
         else:
-            tmp = Bars(name=productName[:100],brand="Rogue",link=productLink[:80],price=productPrice[:12],image="",stock=stockbl)
+            tmp = Bars(name=productName[:100],brand="REP",link=productLink[:80],price=productPrice[2:12],image="",stock=stockbl)
             db.session.add(tmp)
         db.session.commit()
     
@@ -111,11 +107,10 @@ def scrpe():
         else:
             stockbl=True
         tmp2 = Plates.query.filter_by(name=productName[:100]).update(dict(stock=stockbl))
-        #print(tmp2)
         if tmp2>0:
             continue
         else:
-            tmp = Plates(name=productName[:100],brand="Rogue",link=productLink[:80],price=productPrice[:12],image="",stock=stockbl)
+            tmp = Plates(name=productName[:100],brand="REP",link=productLink[:80],price=productPrice[2:12],image="",stock=stockbl)
             db.session.add(tmp)
         db.session.commit()
 
