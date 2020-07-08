c92a29281c80d5d0d6d75f297c30d038b6cb3121
mgla96 <mattgottlieb96@gmail.com>
2020-07-08 16:18:18 -0700

    talisman?

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,16 @@ from config import Config
 
 app = Flask(__name__)
 #Talisman(app)
+csp = {
+    'default-src': '\'self\'',
+    'script-src': '\'self\'',
+}
+talisman = Talisman(
+    app,
+    content_security_policy=csp,
+    content_security_policy_nonce_in=['script-src']
+)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
