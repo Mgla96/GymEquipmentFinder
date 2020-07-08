560ef61a30a3da37e1181dac68c9d989f760d0c6
mgla96 <mattgottlieb96@gmail.com>
2020-07-08 16:04:24 -0700

    looks

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_talisman import Talisman
 from config import Config
 
 app = Flask(__name__)
+Talisman(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
