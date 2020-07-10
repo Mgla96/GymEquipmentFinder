66b9111b2aeac75cba2ea8ac1409b5a1a12fb634
mgla96 <mattgottlieb96@gmail.com>
2020-07-10 13:19:55 -0700

    save

--- app/models.py
+++ app/models.py
@@ -88,3 +88,89 @@ class Plates(db.Model):
     def __repr__(self):
         return '<Plates {}>'.format(self.name)
 
+class Racks(db.Model):
+    __tablename__ = 'Racks'
+    name = db.Column(
+        db.String(100),
+        index=False,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    brand = db.Column(
+        db.String(80),
+        index=True,
+        unique=False,
+        nullable=False
+    )
+    link = db.Column(
+        db.String(160),
+        index=True,
+        unique=True,
+        nullable=False
+    )
+    price = db.Column(
+        db.String(12),
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    image = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    stock = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    __table_args__ = {'extend_existing': True}
+    def __repr__(self):
+        return '<Racks {}>'.format(self.name)
+
+class Dumbbells(db.Model):
+    __tablename__ = 'Dumbbells'
+    name = db.Column(
+        db.String(100),
+        index=False,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    brand = db.Column(
+        db.String(80),
+        index=True,
+        unique=False,
+        nullable=False
+    )
+    link = db.Column(
+        db.String(160),
+        index=True,
+        unique=True,
+        nullable=False
+    )
+    price = db.Column(
+        db.String(12),
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    image = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    stock = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    __table_args__ = {'extend_existing': True}
+    def __repr__(self):
+        return '<Dumbbells {}>'.format(self.name)
+
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
-from app.models import Bars, Plates
+from app.models import Bars, Plates, Dumbbells, Racks
 
 from time import sleep
 import re
@@ -11,13 +11,8 @@ from random import randint
 from bs4 import BeautifulSoup
 
 manager = Manager(app)
-
-@manager.command
-def hello():
-    print("hello")
-@manager.command
-def scrpe():
-    #XMark Barbell
+def XMark():
+    #Barbell ..
     response = get('https://www.xmarkfitness.com/bars/?limit=80')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     posts = html_soup.find_all('li',class_='Odd') #<li class="result-row">
@@ -47,8 +42,7 @@ def scrpe():
                 tmp = Bars(name=productName,brand="XMark",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
                 db.session.add(tmp)
                 db.session.commit()
-
-    #XMark Plates
+    #Plates
     response = get('https://www.xmarkfitness.com/free-weights/weight-plates/?limit=80')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     posts = html_soup.find_all('li',class_='Odd') #<li class="result-row">
@@ -77,8 +71,38 @@ def scrpe():
             else:
                 tmp = Plates(name=productName,brand="XMark",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
                 db.session.add(tmp)
-                db.session.commit()   
+                db.session.commit()  
+    #Dumbbell
+    response=get("https://www.xmarkfitness.com/free-weights/dumbbells/?limit=80")
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    dumbbells = html_soup.find_all('li',class_='Odd') #<li class="result-row">
+    dumbbells2 = html_soup.find_all('li',class_='Even')
+    dumbbells = dumbbells + dumbbells2
+    for dumbbell in dumbbells:
+        a=dumbbell.find("a",class_="pname")
+        productName=a.text
+        productLink=a.get("href")
+        productPrice=dumbbell.find("em",class_="p-price").text
+        outStock=dumbbell.find("a",class_="icon-Out")
+        inStock=dumbbell.find("a",class_="icon-Add")
+        if inStock:
+            inStock="In Stock"
+        elif outStock:
+            inStock=outStock.text
+        else:
+            inStock="Out of stock"
+        if productName:
+            if productPrice[0]=="$":
+                productPrice=productPrice[1:]
+            tmp2 = db.session.query(Dumbbells).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock   
+            else:
+                tmp = Dumbbells(name=productName,brand="XMark",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
 
+def REP():
     #REP Men's 20KG Barbell - should be good
     response = get('https://www.repfitness.com/bars-plates/olympic-bars')
     html_soup = BeautifulSoup(response.text, 'html.parser')
@@ -92,21 +116,19 @@ def scrpe():
         ownPage = get(productLink)
         html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
         info = html_soup2.find('p',class_='availability')
-        stockbl = info.find('span').text
-        if not stockbl:
-            stockbl=""
+        inStock = info.find('span').text
+        if not inStock:
+            inStock=""
         if productName:
             if productPrice[0]=="$":
                 productPrice=productPrice[1:]
             tmp2 = db.session.query(Bars).filter_by(name=productName).first()
             if tmp2:
-                tmp2.stock=stockbl
-                db.session.commit()
+                tmp2.stock=inStock
             else:
-                tmp = Bars(name=productName,brand="REP",link=productLink[:160],price=productPrice[1:12],image="",stock=stockbl)
+                tmp = Bars(name=productName,brand="REP",link=productLink[:160],price=productPrice[1:12],image="",stock=inStock)
                 db.session.add(tmp)
-                db.session.commit()
-    
+            db.session.commit()
     #REP Plates
     response = get('https://www.repfitness.com/catalogsearch/result/index/?cat=113&q=plates')
     html_soup = BeautifulSoup(response.text, 'html.parser')
@@ -121,22 +143,48 @@ def scrpe():
         html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
         info = html_soup2.find('p',class_='availability')
         inStock = info.find('span').text
-        if not stockbl:
-            stockbl=""
+        if not inStock:
+            inStock=""
         if productName:
             if productPrice[0]=="$":
                 productPrice=productPrice[1:]
             tmp2 = db.session.query(Plates).filter_by(name=productName).first()
             if tmp2:
-                tmp2.stock=stockbl
-                db.session.commit()
+                tmp2.stock=inStock
             else:
-                tmp = Plates(name=productName,brand="REP",link=productLink[:160],price=productPrice[:12],image="",stock=stockbl)
+                tmp = Plates(name=productName,brand="REP",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
                 db.session.add(tmp)
-                db.session.commit()
+            db.session.commit()
     
-
-    #titan
+    #REP dumbbell
+    response = get('https://www.repfitness.com/conditioning/strength-equipment/dumbbells')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    plates = html_soup.find_all('li',class_='item') #<li class="result-row">
+    for plate in plates:
+        prodInfo = plate.find('h2', class_='product-name')
+        pricecont = plate.find('div', class_='price-container')
+        productName = prodInfo.text[:-2]
+        productLink = prodInfo.find('a').get('href')
+        productPrice = pricecont.find('span',class_='price').text[1::]
+        ownPage = get(productLink)
+        html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
+        info = html_soup2.find('p',class_='availability')
+        inStock = info.find('span').text
+        if not inStock:
+            inStock=""
+        if productName:
+            if productPrice[0]=="$":
+                productPrice=productPrice[1:]
+            tmp2 = db.session.query(Dumbbells).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock     
+            else:
+                tmp = Dumbbells(name=productName,brand="REP",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
+    
+def Titan():
+    #Weight Plates
     response = get('https://www.titan.fitness/strength/weight-plates/?size=90')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     plates = html_soup.find_all('div',class_='product') #<li class="result-row">
@@ -170,12 +218,11 @@ def scrpe():
             tmp2 = db.session.query(Plates).filter_by(name=productName).first()
             if tmp2:
                 tmp2.stock=inStock
-                db.session.commit()
             else:
                 tmp = Plates(name=productName,brand="Titan",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
                 db.session.add(tmp)
-                db.session.commit()
-
+            db.session.commit()
+    #Barbells
     response = get('https://www.titan.fitness/strength/barbells/?size=90')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     bars = html_soup.find_all('div',class_='product') #<li class="result-row">
@@ -208,21 +255,59 @@ def scrpe():
                 inStock = page_container.text
             tmp2 = db.session.query(Bars).filter_by(name=productName).first()
             if tmp2:
-                tmp2.stock=inStock
-                db.session.commit()
+                tmp2.stock=inStock 
             else:
                 tmp = Bars(name=productName,brand="Titan",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
                 db.session.add(tmp)
-                db.session.commit()
-@manager.command
-def scrpe2():
-    #Fringe Sport Plates
+            db.session.commit()
+    #Dumbbells
+    response = get('https://www.titan.fitness/strength/dumbbells/')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    dumbbells = html_soup.find_all('div',class_='product') #<li class="result-row">
+    for dumbbell in dumbbells:
+        tst = dumbbell.find('div',class_='image-container')
+        prd = tst.find('img', class_='tile-image')
+        productName = prd.get('title')
+        productLink = 'https://www.titan.fitness/'+tst.find('a',class_='gtm-product-list').get('href')
+        prod = dumbbell.find('span',class_='value')
+        if prod:
+            productPrice=prod.get('content')
+        else:
+            productPrice="?"
+        if productName and productPrice!="?":
+            if productPrice[0]=="$":
+                productPrice=productPrice[1:]
+            ownPage = get(productLink)
+            html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
+            page_container = html_soup2.find('span',class_='strong')
+            if not page_container:
+                inStock = html_soup2.find('span',class_="in-stock")
+                if inStock:
+                    inStock=inStock.text
+                    if inStock!="In Stock":
+                        inStock="Out of Stock"
+                else:
+                    inStock="Out of Stock"
+            else:
+                inStock = page_container.text
+            tmp2 = db.session.query(Dumbbells).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock 
+            else:
+                tmp = Dumbbells(name=productName,brand="Titan",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
+        
+        
+
+
+def Fringe():
+    #Plates 
     response = get('https://www.fringesport.com/collections/bumper-plates/?size=90')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     plates = html_soup.find_all("div",class_="odd")
     plates2 = html_soup.find_all("div",class_="even")
     plates=plates+plates2
-
     for plate in plates:
         productName = plate.find("a").title
         productLink = "https://www.fringesport.com"+plate.find("a").get("href")
@@ -259,19 +344,16 @@ def scrpe2():
         tmp2 = db.session.query(Plates).filter_by(name=productName).first()
         if tmp2:
             tmp2.stock=inStock
-            db.session.commit()
         else:
             tmp = Plates(name=productName,brand="Fringe Sport",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
             db.session.add(tmp)
-            db.session.commit()
-
-    #Fringe Sport Barbells
-    response = get('https://www.fringesport.com/collections/bumper-plates/?size=90')
+        db.session.commit()
+    #Barbells
+    response = get('https://www.fringesport.com/collections/barbells/?size=90')
     html_soup = BeautifulSoup(response.text, 'html.parser')
     bar = html_soup.find_all("div",class_="odd")
     bar2 = html_soup.find_all("div",class_="even")
     bars=bar+bar2
-
     for bar in bars:
         productName = bar.find("a").title
         productLink = "https://www.fringesport.com"+bar.find("a").get("href")
@@ -308,11 +390,24 @@ def scrpe2():
         tmp2 = db.session.query(Bars).filter_by(name=productName).first()
         if tmp2:
             tmp2.stock=inStock
-            db.session.commit()
         else:
             tmp = Bars(name=productName,brand="Fringe Sport",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
             db.session.add(tmp)
-            db.session.commit()   
+        db.session.commit()  
+     
+
+@manager.command
+def hello():
+    print("hello")
+@manager.command
+def scrpe():
+    XMark()
+    REP()
+    Titan()    
+@manager.command
+def scrpe2():
+    Fringe()
+    
 #manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
