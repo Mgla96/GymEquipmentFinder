1e45190e4b1b691093ceb78390618b87282f26de
mgla96 <mattgottlieb96@gmail.com>
2020-07-12 09:49:09 -0700

    img compression

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from app import app
 from .models import db, Bars, Plates, Racks, Dumbbells
 
@@ -14,3 +14,9 @@ def index():
     }
     return render_template('index.html',title='Home',user=user,bars=Bars.query.all(),plates=Plates.query.all(),dumbbells=Dumbbells.query.all(),racks=Racks.query.all())
 
+"""
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+"""
