5f64e32819ab44c815ed7d9c0c09a7943fc8c87f
mgla96 <mattgottlieb96@gmail.com>
2020-07-12 17:29:06 -0700

    sitemap and robots.txt

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, request, make_response
 from app import app
 from .models import db, Bars, Plates, Racks, Dumbbells
 
@@ -14,9 +14,7 @@ def index():
     }
     return render_template('index.html',title='Home',user=user,bars=Bars.query.all(),plates=Plates.query.all(),dumbbells=Dumbbells.query.all(),racks=Racks.query.all())
 
-"""
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
-"""
