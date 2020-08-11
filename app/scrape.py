f15215bbd6725a8599d3852101dd26e97560db99
mgla96 <mattgottlieb96@gmail.com>
2020-08-11 15:22:56 -0700

    format

---
+++ app/scrape.py
@@ -0,0 +1,169 @@
+from time import sleep
+import re
+from requests import get 
+import numpy as np
+from random import randint
+from .models import db, Bars, Plates
+from bs4 import BeautifulSoup
+def retry():
+    wait = randint(0,2)
+    sleep(wait)
+
+def scrpe():
+    #REP Men's 20KG Barbell
+    response = get('https://www.repfitness.com/bars-plates/olympic-bars')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    bars = html_soup.find_all('li',class_='item')
+    for bar in bars:
+        prodInfo = bar.find('h2', class_='product-name')
+        pricecont = bar.find('div', class_='price-container')
+        productName = prodInfo.text[:100]
+        productLink = prodInfo.find('a').get('href')
+        productPrice = pricecont.find('span',class_='price').text
+        ownPage = get(productLink)
+        html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
+        info = html_soup2.find('p',class_='availability')
+        availability = info.find('span').text #availability
+        stockbl=True
+        if availability:
+            if availability == "Out of stock":
+                stockbl=False
+        if productName:
+            tmp2 = db.session.query(Bars).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=stockbl
+            else:
+                tmp = Bars(name=productName,brand="REP",link=productLink[:160],price=productPrice[1:12],image="",stock=stockbl)
+                db.session.add(tmp)
+            db.session.commit()
+    
+    #REP Plates
+    response = get('https://www.repfitness.com/catalogsearch/result/index/?cat=113&q=plates')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    plates = html_soup.find_all('li',class_='item') #<li class="result-row">
+    for plate in plates:
+        prodInfo = plate.find('h2', class_='product-name')
+        pricecont = plate.find('div', class_='price-container')
+        productName = prodInfo.text[:-2]
+        productLink = prodInfo.find('a').get('href')
+        productPrice = pricecont.find('span',class_='price').text[1::]
+        ownPage = get(productLink)
+        html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
+        info = html_soup2.find('p',class_='availability')
+        inStock = info.find('span').text
+        stockbl=True
+        if inStock:
+            stockbl=False
+        if productName:
+            tmp2 = db.session.query(Plates).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=stockbl
+            else:
+                tmp = Plates(name=productName,brand="REP",link=productLink[:160],price=productPrice[:12],image="",stock=stockbl)
+                db.session.add(tmp)
+            db.session.commit()
+    
+
+    #Rogue Mens 20kg Barbells
+    response = get('https://www.roguefitness.com/weightlifting-bars-plates/barbells/mens-20kg-barbells?limit=80')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    posts = html_soup.find_all('li',class_='item') 
+    for post in posts:
+        search_header = post.find('div', class_='product-details')
+        productName = search_header.find('h2',class_='product-name').text[:100]
+        productLink = search_header.find('a').get('href')
+        productPrice = search_header.find('span',class_='price').text[1::]
+        ownPage = get(productLink)
+        html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
+        page_container = html_soup2.find('div',class_='main-container')
+        avail = page_container.find('div',class_='bin-stock-availability')
+        inStock=True
+        if avail.find('div',class_='bin-signup-dropper'):
+            inStock=False
+        if productName:
+            tmp2 = db.session.query(Bars).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock
+            else:
+                tmp = Bars(name=productName,brand="Rogue",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
+
+    #Rogue Plates
+    response = get('https://www.roguefitness.com/weightlifting-bars-plates/bumpers')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    plates = html_soup.find_all('li',class_='item')
+    for plate in plates:
+        search_header = plate.find('div', class_='product-details')
+        productName = search_header.find('h2',class_='product-name').text[:100]
+        productLink = search_header.find('a').get('href')     
+        productPrice = search_header.find('span',class_='price').text
+        ownPage = get(productLink)
+        html_soup2 = BeautifulSoup(ownPage.text, 'html.parser')
+        page_container = html_soup2.find('div',class_='main-container')
+        avail = page_container.find('div',class_='bin-stock-availability')
+        inStock=True
+        if avail.find('div',class_='bin-signup-dropper'):
+            inStock=False
+        if productName:
+            tmp2 = db.session.query(Plates).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock
+            else:
+                tmp = Plates(name=productName,brand="Rogue",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
+
+    response = get('https://www.titan.fitness/strength/weight-plates/?size=90')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    plates = html_soup.find_all('div',class_='product') #<li class="result-row">
+    for plate in plates:
+        tst = plate.find('div',class_='image-container')
+        prd = tst.find('img', class_='tile-image')
+        productName = prd.get('title')
+        img = prd.get('src')
+        productLink = 'https://www.titan.fitness/'+tst.find('a',class_='gtm-product-list').get('href')
+        prod = plate.find('span',class_='value')
+        if prod:
+            productPrice=prod.get('content')
+        else:
+            productPrice="0"
+        inStock=True
+        if plate.find('div',class_='oos'):
+            inStock=False
+        if productName:
+            tmp2 = db.session.query(Plates).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock
+            else:
+                tmp = Plates(name=productName,brand="Titan",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
+
+    response = get('https://www.titan.fitness/strength/barbells/?size=90')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    bars = html_soup.find_all('div',class_='product') #<li class="result-row">
+    for bar in bars:
+        tst = bar.find('div',class_='image-container')
+        prd = tst.find('img', class_='tile-image')
+        productName = prd.get('title')
+        productLink = 'https://www.titan.fitness/'+tst.find('a',class_='gtm-product-list').get('href')
+        img = prd.get('src')
+        prod = plate.find('span',class_='value')
+        if prod:
+            productPrice=prod.get('content')
+        else:
+            productPrice="?"
+        inStock=True
+        if bar.find('div',class_='oos'):
+            inStock=False
+        if productName:
+            tmp2 = db.session.query(Bars).filter_by(name=productName).first()
+            if tmp2:
+                tmp2.stock=inStock
+            else:
+                tmp = Bars(name=productName,brand="Titan",link=productLink[:160],price=productPrice[:12],image="",stock=inStock)
+                db.session.add(tmp)
+            db.session.commit()
+        
+scrpe()
