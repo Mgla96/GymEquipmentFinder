7b7cfe6aaa6e1d3ac8a491de8a7105d32ea35b85
mgla96 <mattgottlieb96@gmail.com>
2020-08-13 23:56:02 -0700

    https

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,9 @@ def before_request():
         urlparts_list = list(urlparts)
         urlparts_list[1] = 'https://www.weightsinstock.com'
         return redirect(urlunparse(urlparts_list), code=301)
+    if request.url.startswith('http://'):
+        url=request.url.replace('http://','https://',1)
+        return redirect(url,code=301)
 
 @app.route('/')
 @app.route('/index')
