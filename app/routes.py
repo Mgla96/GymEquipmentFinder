aea59d4f17f3bd7130d4b64a99f89d5f5687aa45
mgla96 <mattgottlieb96@gmail.com>
2020-08-18 22:58:45 -0700

    finding root issue

--- app/routes.py
+++ app/routes.py
@@ -15,10 +15,11 @@ def before_request():
         urlparts_list = list(urlparts)
         urlparts_list[1] = 'https://www.weightsinstock.com'
         return redirect(urlunparse(urlparts_list), code=301)
-    '''
     if request.url.startswith('http://'):
         url=request.url.replace('http://','https://',1)
         return redirect(url,code=301)
+    '''
+    pass
 
 @app.route('/')
 @app.route('/index')
